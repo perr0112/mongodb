6bf4fe7122d8ab2d992d160d7be8a08c36a949fc
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2025-12-18 11:10:12 +0100

    docs: add Swagger documentation setup for the API

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -29,6 +29,27 @@ export async function closeDB() {
     }
 }
 
+export const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Le Carnet Gourmand API",
+      version: "0.1.0",
+      description:
+        "API Documentation for 'Le Carnet Gourmand' project, built with Express and documented with Swagger.",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Development Team",
+        email: "prtclement.ctc@gmail.com",
+      },
+    },
+  },
+    apis: ["./src/docs/*.js", "./src/docs/**/*.js"],
+}
+
 process.on("SIGINT", async () => {
     await closeDB()
     process.exit(0)
