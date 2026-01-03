2d08ae7cd0a095b561d35bc7fa004b184b306ffa
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2026-01-03 02:34:06 +0100

    feat(frontend): authentication

--- frontend/src/pages/authentication/Authentication.jsx
+++ frontend/src/pages/authentication/Authentication.jsx
@@ -4,13 +4,33 @@ import Login from "./Login"
 import Register from "./Register"
 
 import { MainLogo } from "../../components/icons"
+import Header from "../../components/navbar/header/Header"
+
+const AUTHENTICATION_LABELS = [
+    {
+        label: "Entrées",
+        color: "#4B2CE3",
+    },
+    {
+        label: "Plats",
+        color: "#FF5E29",
+    },
+    {
+        label: "Desserts",
+        color: "#00804A",
+    },
+    {
+        label: "Cocktails",
+        color: "#4F060B",
+    },
+]
 
 const Authentication = () => {
 
-    const [mode, setMode] = useState('login')
+    const [mode, setMode] = useState("login")
 
     const toggleMode = useCallback(() => {
-        setMode(mode === 'login' ? 'register' : 'login')
+        setMode(mode === "login" ? "register" : "login")
     }, [mode])
     
     return (
@@ -18,6 +38,33 @@ const Authentication = () => {
             <div className="authentication__container">
                 {/* Left Side */}
                 <div className="authentication__left">
+                    {/* Labels */}
+                    <div className="authentication__labels">
+                        <div className="authentication__labels-group">
+                            {AUTHENTICATION_LABELS.slice(0, 2).map(({ label, color }) => (
+                                <div
+                                    key={label}
+                                    className="authentication__label"
+                                    style={{ backgroundColor: color }}
+                                >
+                                    {label}
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="authentication__labels-group">
+                            {AUTHENTICATION_LABELS.slice(2).map(({ label, color }) => (
+                                <div
+                                    key={label}
+                                    className="authentication__label"
+                                    style={{ backgroundColor: color }}
+                                >
+                                    {label}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
                     <div className="book-cover">
                         <div className="book-cover__content">
                             <MainLogo color="#F3EDE5" />
@@ -32,7 +79,12 @@ const Authentication = () => {
 
                 {/* Right Side */}
                 <div className="authentication__right">
-                    <p>form...</p>
+                    <Header mode="authentication" />
+
+                    <div className="authentication__form-container">
+                        {mode === 'login' && <Login toggleMode={toggleMode} />}
+                        {mode === 'register' && <Register toggleMode={toggleMode} />}
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/pages/authentication/Login.jsx
+++ frontend/src/pages/authentication/Login.jsx
@@ -1,6 +1,96 @@
-const Login = () => {
+import { useContext, useState } from "react"
+
+import toast, { Toaster } from "react-hot-toast"
+
+import Input from "../../components/form/Input"
+import Button from "../../components/ui/button"
+import Link from "../../components/ui/Link"
+
+import { login } from "../../services/index"
+import UserContext from "../../contexts/user"
+
+const Login = ({ toggleMode }) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [loading, setLoading] = useState(false)
+
+    const { saveUser } = useContext(UserContext)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setLoading(true)
+        const res = login(email, password)
+        res.then(response => {
+            console.log("resp", response)
+
+            const { data, status } = response
+
+            setTimeout(() => {
+                setLoading(false)
+                if (status === 200 && data.success) {
+                    toast.success("Connexion réussie")
+                    saveUser(data.user)
+                } else {
+                    toast.error(data.message || "Erreur lors de la connexion")
+                }
+            }, 1000)
+        }
+        ).catch(error => {
+            setTimeout(() => {
+                setLoading(false)
+                toast.error(error.response.data.message || "Erreur lors de la connexion")
+            }, 1000)
+        })
+    }
+
     return (
-        <div>Login Page</div>
+        <div>
+            <Toaster />
+
+            <form className="authentication__form-login" onSubmit={handleSubmit}>
+
+                <div className="inputs__group">
+                    <Input
+                        type="email"
+                        name="email"
+                        label="Email"
+                        placeholder="Saisissez votre email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required={true}
+                    />
+                    <Input
+                        type="password"
+                        name="password"
+                        label="Mot de passe"
+                        placeholder="Saisissez votre mot de passe"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required={true}
+                    />
+                </div>
+
+                <div className="actions__group">
+                    <Button
+                        label={loading ? "Connexion..." : "Se connecter"}
+                        type="submit"
+                        variant="primary"
+                        style={{
+                            width: "100%",
+                            pointerEvents: loading ? "none" : "auto",
+                            opacity: loading ? 0.7 : 1,
+                        }}
+                    />
+
+                    <div className="actions__link">
+                        <Link label="S'inscrire ici" type="button" onClick={toggleMode} />
+                        <Link href="/" label="Mot de passe oublié" type="button" />
+                    </div>
+                </div>
+
+            </form>
+        </div>
     )
 }
 
