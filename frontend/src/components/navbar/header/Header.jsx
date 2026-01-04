1bbf285b2821ee0711bd671810f9799e1caa77c5
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2026-01-04 05:19:53 +0100

    refactor(Header): enhance navigation and user actions in Header component

--- frontend/src/components/navbar/header/Header.jsx
+++ frontend/src/components/navbar/header/Header.jsx
@@ -1,11 +1,95 @@
+import { useContext } from "react"
+
 import { FaceLogo, HeaderLogo } from "../../icons"
 
+import UserContext from "../../../contexts/user/UserContext"
+import LinkComponent from "../../ui/Link"
+import Button from "../../ui/button"
+import { useLocation, useNavigate } from "react-router-dom"
+
+console.log("PROVIDER CONTEXT", UserContext)
+
 const Header = ({ mode = "main" }) => {
+    const { user, logout } = useContext(UserContext)
+
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const handleAuthNavigation = (mode) => {
+        navigate("/auth", { state: { modeProvided: mode } })
+    }
+
     // Mode = 'main' | 'authentication'
     return (
-        <header className="navbar__header">
+        <header className={`navbar__header ${mode === "main" ? "fixed" : ""}`}>
             {mode === "main" && (
-                <div>Mode: main</div>
+                <div className="navbar__header-main">
+                    <div className="navbar__header-logo">
+                        <HeaderLogo />
+                        <h3>Le Carnet Gourmand</h3>
+                    </div>
+
+                    <div className="navbar__header-actions">
+                        <div className="actions__links">
+                            {user ? (
+                                <>
+                                    <LinkComponent
+                                        type="link"
+                                        href="/"
+                                        active={location.pathname === "/"}
+                                        label="Accueil"
+                                        variant="secondary"
+                                    />
+
+                                    <LinkComponent
+                                        type="link"
+                                        href="/recipes"
+                                        active={location.pathname === "/recipes"}
+                                        label="Recettes disponibles"
+                                        variant="secondary"
+                                    />
+
+                                    <Button
+                                        label="Ajouter ma recette"
+                                        type="button"
+                                        variant="primary"
+                                    />
+
+                                    <div className="user-avatar">
+                                        <Button
+                                            label="!"
+                                            type="button"
+                                            variant="primary"
+                                        />
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <LinkComponent
+                                        type="link"
+                                        href="/"
+                                        active={location.pathname === "/"}
+                                        label="Accueil"
+                                        variant="secondary"
+                                    />
+                                    <LinkComponent
+                                        type="link"
+                                        href="/recipes"
+                                        active={location.pathname === "/recipes"}
+                                        label="Recettes disponibles"
+                                        variant="secondary"
+                                    />
+                                    <Button
+                                        label="Se connecter"
+                                        type="button"
+                                        variant="primary"
+                                        onClick={() => handleAuthNavigation("login")}
+                                    />
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </div>
             )}
 
             {mode === "authentication" && (
