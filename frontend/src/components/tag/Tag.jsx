3c59800fdb113b41ca8343b79a2ac2f144a9bc61
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2026-01-04 05:19:33 +0100

    refactor(tag): types added (for later) and enhance Tag component with props

--- frontend/src/components/tag/Tag.jsx
+++ frontend/src/components/tag/Tag.jsx
@@ -1,6 +1,19 @@
-const Tag = ({ text, type }) => {
+export const CATEGORY_TYPES = {
+  entree: "blue",
+  plat: "orange",
+  dessert: "green",
+  cocktail: "brown",
+  facile: "green-light",
+  intermediaire: "orange-light",
+}
+
+const Tag = ({ text, type, style, onClick }) => {
     return (
-        <div className={`tag tag-${type}`}>
+        <div
+            className={`tag tag-${type}`}
+            style={style}
+            onClick={onClick}
+        >
             {text}
         </div>
     )
