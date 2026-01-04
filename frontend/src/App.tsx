409d0113bbdb0cfed22c34f87af29622291ac969
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2026-01-04 05:20:07 +0100

    refactor(App): restructure App component for better route and header visibility

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,28 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
 
-// @ts-ignore
 import Authentication from "./pages/authentication/Authentication"
-// @ts-ignore
-import { UserProvider } from "./contexts/user/Provider.jsx"
+import Home from "./pages/Home"
+
+import Header from "./components/navbar/header/Header.jsx"
+import Recipes from "./pages/recipes/Recipes.jsx"
 
 function App() {
+  const location = useLocation()
+
   return (
-    <UserProvider>
+    <>
+      <Toaster />
+
+      {/* According to the design, the header has to be hidden on the authentication page */}
+      {location.pathname !== "/auth" && <Header />}
+
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
         <Route path="/auth" element={<Authentication />} />
       </Routes>
-    </UserProvider>
+    </>
   )
 }
 
