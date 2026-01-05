6e03c6d57dc62b15bb443deb7c34e84d15a40844
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2026-01-05 15:27:24 +0100

    feat(card): truncate for title and add rating

--- frontend/src/components/ui/Card.jsx
+++ frontend/src/components/ui/Card.jsx
@@ -1,19 +1,22 @@
+import { truncate } from "../../utils/string"
 import Tag from "../tag/Tag"
 
-const Card = ({ isLink, title, slug, categories, views, coverImg, style }) => {
+const Card = ({ isLink, title, slug, categories, views, rating, countRatings, coverImg, style }) => {
     return (
         <div className="card__container" style={style}>
 
             <div className="card__image">
                 <img src={coverImg} alt={title} />
-
-                { isLink && (
-                    <a href={`/articles/${slug}`} className="card__overlay-link"></a>
-                )}
             </div>
 
+            { isLink && (
+                <a href={`/articles/${slug}`} className="card__overlay-link"></a>
+            )}
+
             <div className="card__info">
-                <p className="card__title">{title}</p>
+                <p className="card__title">
+                    {truncate(title, 40)}
+                </p>
                 <div className="card__categories">
                     <div className="card__categories">
                         {categories.map(({ label }, index) => (
@@ -29,9 +32,13 @@ const Card = ({ isLink, title, slug, categories, views, coverImg, style }) => {
                         ))}
                     </div>
                 </div>
-                <div className="card__views">
-                    <p>{views} vues</p>
-                </div>
+            </div>
+
+            <div className="card__details">
+                <p className="card__rating">
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill="currentColor"></path></svg>
+                    {rating} / 5 ({countRatings} avis)</p>
+                <p>{views} vues</p>
             </div>
         </div>
     )
