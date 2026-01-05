5f00f99897e2862e99cbdf53413bcb9b1a8047a2
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2026-01-05 14:56:47 +0100

    feat(recipes): real recipes data added for fixtures

---
+++ backend/src/fixtures/data/dataArticles.js
@@ -0,0 +1,123 @@
+/**
+ * Ces recettes proviennent du dépôt github suivant :
+ * https://github.com/kodecocodes/recipes
+ */
+
+export const RECIPES = [
+    {
+        title: "Rôti de boeuf à la mijoteuse",
+        content: `
+            Ingrédients:
+            - 1 rôti de bœuf
+            - 1 paquet de mélange pour sauce brune
+            - 1 paquet de mélange pour vinaigrette italienne sèche
+            - 1 paquet de mélange pour vinaigrette ranch sèche
+            - 1/2 tasse d'eau
+        
+            Placez le rôti de bœuf dans la mijoteuse.
+            Mélangez les mélanges secs dans un bol et saupoudrez-les sur le rôti.
+            Versez l'eau autour du rôti.
+            Faites cuire à feu doux pendant 7 à 9 heures..`,
+        categories: ["Plats", "Intermédiaire"],
+        coverImg: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/27/20/8/picVfzLZo.jpg",
+    },
+    {
+        title: "Asperges rôties",
+        content: `
+            Ingrédients:
+            - 450 g d'asperges
+            - 1 1/2 c. à soupe d'huile d'olive
+            - 1/2 c. à café de sel kasher
+
+            Préchauffez le four à 220°C.
+            Coupez la partie dure des asperges et jetez-la.
+            Avec un épluche-légumes, retirez la peau sur les 5 cm inférieurs.
+            Placez les asperges sur une plaque recouverte de papier aluminium et arrosez d'huile.
+            Saupoudrez de sel et mélangez avec vos mains pour bien enrober.
+            Faites rôtir 10 à 15 minutes selon l'épaisseur des tiges.
+            Elles doivent être tendres et légèrement dorées.`,
+        categories: ["Entrées", "Facile"],
+        coverImg: "http://img.sndimg.com/food/image/upload/w_266/v1/img/recipes/50/84/7/picMcSyVd.jpg",
+    },
+    {
+        title: "Lentilles au curry et riz",
+        content: `
+            Ingrédients:
+            - 1 l de bouillon de bœuf
+            - 1 tasse de lentilles vertes sèches
+            - 1/2 tasse de riz basmati
+            - 1 c. à café de curry en poudre
+            - 1 c. à café de sel
+
+            Portez le bouillon à ébullition.
+            Ajoutez le curry et le sel.
+            Faites cuire les lentilles 20 minutes.
+            Ajoutez le riz et laissez mijoter encore 20 minutes.
+            Servez chaud.`,
+        categories: ["Plats", "Facile"],
+        coverImg: "https://marceletfils.com/cdn/shop/articles/Dhal_de_lentilles_corail_1_600x600_crop_center.png?v=1677061994",
+    },
+    {
+        title: "Pizza Big Night",
+        content: `
+            Ingrédients:
+            - 5 c. à thé de levure
+            - 5 tasses de farine
+            - 4 c. à soupe d'huile végétale
+            - 2 c. à soupe de sucre
+            - 2 c. à thé de sel
+            - 2 tasses d'eau chaude
+            - 1/4 tasse de sauce à pizza
+            - 3/4 tasse de mozzarella
+
+            Mélangez l'eau chaude avec la levure et laissez reposer 15 minutes.
+            Ajoutez huile, sucre, sel et farine, laissez reposer 1 heure.
+            Pétrissez la pâte et étalez-la sur un plat.
+            Étalez la sauce et le fromage.
+            Ajoutez les garnitures optionnelles.
+            Faites cuire à 200°C pendant 15 minutes.`,
+        categories: ["Plats", "Intermédiaire"],
+        coverImg: "http://upload.wikimedia.org/wikipedia/commons/c/c7/Spinach_pizza.jpg",
+    },
+    {
+        title: "Courge poivrée farcie aux cranberries et pommes",
+        content: `
+            Ingrédients:
+            - 2 courges poivrées
+            - 1 tasse d'eau bouillante
+            - 2 pommes coupées en dés
+            - 1/2 tasse de cranberries séchées
+            - 1 c. à café de cannelle
+            - 2 c. à soupe de beurre fondu
+
+            Coupez les courges en deux et retirez les graines.
+            Placez-les face coupée vers le bas dans un plat allant au four.
+            Versez 1/4 de pouce d'eau dans le plat.
+            Faites cuire 30 minutes à 175°C.
+            Mélangez le reste des ingrédients dans un bol.
+            Remplissez les courges avec ce mélange.
+            Faites cuire 30 à 40 minutes supplémentaires jusqu'à tendreté.`,
+        categories: ["Plats", "Intermédiaire"],
+        coverImg: "http://elanaspantry.com/wp-content/uploads/2008/10/acorn_squash_with_cranberry.jpg",
+    },
+    {
+        title: "Yorkshire Puddings",
+        content: `
+            Ingrédients:
+            - 200 g de farine
+            - 3 œufs
+            - 300 ml de lait
+            - 3 c. à soupe d'huile végétale
+
+            Mélangez la farine avec un peu d'assaisonnement dans un grand bol.
+            Ajoutez les œufs un par un.
+            Incorporez le lait jusqu'à obtenir une pâte lisse.
+            Réfrigérez 30 minutes.
+            Préchauffez le four à 220°C.
+            Versez l'huile dans les moules.
+            Chauffez 5 minutes puis versez la pâte.
+            Faites cuire 30 minutes jusqu'à ce qu'ils soient dorés et gonflés.`,
+        categories: ["Desserts", "Intermédiaire"],
+        coverImg: "http://upload.wikimedia.org/wikipedia/commons/f/f9/Yorkshire_Pudding.jpg",
+    },
+]
